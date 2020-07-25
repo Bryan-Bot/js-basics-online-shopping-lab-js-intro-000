d4615fb6a7a6bd35359b4d2c56e277d33e5b6429
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-07-25 22:00:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,30 +20,19 @@ function addToCart(item) {
 function viewCart() {
   // write your code here
 
-  if(!cart.length/*checks if cart is zero*/){
-      return "Your shopping cart is empty.";
+    if (cart.length ===0){
+      return "Your shopping cart is empty."
     }
-    var ans = "In your cart, you have ";
-    for(i=0; i<cart.length; i++){
-      for(var j in cart[i]){
-        if(i === 0){
-          ans += `${j} at $${cart[i][j]}`;
-          continue;
-        }
-        if(i === cart.length - 1){
-          if(cart.length > 2){
-            ans += ',';
-          }
-          ans += ` and ${j} at $${cart[i][j]}`;
-          continue;
-        }
-        ans += `, ${j} at $${cart[i][j]}`;
+    var say = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}`
+    if(cart.length === 1){
+      return `${say}.`
+    }
+      for(let i = 1; i < cart.length ;i++){
+        // for loops iterates over arrays length
+      //  var say = `${say}, and ${cart[i].itemName} at $${cart[i].itemPrice}.`
+      //  return say
       }
-    }
-    ans += ".";
-    console.log(ans);
-    return ans;
-  }
+
 
 
 function total() {
@@ -51,7 +40,7 @@ function total() {
   var sum = cart.itemPrice
   var hex = 0
   for (var i=0; i<cart; i++){
-    hex = hex + cart.itemPrice[i];
+    hex = hex + cart[i].itemPrice;
     return hex
   }
 }
