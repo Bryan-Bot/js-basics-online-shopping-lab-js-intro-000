db3518934c579aac1a59ebc49581def09a994b80
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-07-25 23:29:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,7 +61,8 @@ function placeOrder(cardNumber) {
   if (cardNumber = undefined){
       return `Sorry, we don't have a credit card on file for you.`
   }else{
+  var cost = total()
   cart.splice(0,cart.length)
-  return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+  return `Your total cost is $${cost}, which will be charged to the card ${cardNumber}.`
   }
 }
