68b4fda6a83514426319ee6a55ed7fe03b4e39c9
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-07-25 22:01:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,18 +20,7 @@ function addToCart(item) {
 function viewCart() {
   // write your code here
 
-    if (cart.length ===0){
-      return "Your shopping cart is empty."
-    }
-    var say = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}`
-    if(cart.length === 1){
-      return `${say}.`
-    }
-      for(let i = 1; i < cart.length ;i++){
-        // for loops iterates over arrays length
-      //  var say = `${say}, and ${cart[i].itemName} at $${cart[i].itemPrice}.`
-      //  return say
-      }
+}
 
 
 
